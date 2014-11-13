5b688064b11d8b24d91745ef4b837eba6019f1f6
Brad Sawicki <git@bradsawicki.com>
2014-11-13 10:38:58 -0600

    Ignore update / EP links

--- views/body.php
+++ views/body.php
@@ -2,11 +2,15 @@
   <div class="wrapper">
 
     <iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=727538613/size=large/bgcol=faf9d9/linkcol=ee333d/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://ancientfriends.bandcamp.com/album/power-mountain">Power Mountain by Ancient Friends</a></iframe>
+    
+    <h2>New EP: <em>Power Mountain</em></h2>
+    <p class="album-links"><a href="https://ancientfriends.bandcamp.com/album/power-mountain">Download</a> / <a href=" https://itunes.apple.com/us/album/power-mountain-ep/id939746794">iTunes</a> / <a href=" http://open.spotify.com/album/7G30izATkl5R714ajAlmSn">Spotify</a></p>
   
-    <h2>Next Show</h2>    
+    <h3>Next Show</h3>    
     <div class="show">
-      <h3><em>Power Mountain</em> EP Release Party</h3>
+      <h4><em>Power Mountain</em> EP Release Party</h4>
       <p>November 22 @ <a href="http://www.lh-st.com/Shows/11-22-2014+Forgotten+Species">Schubas</a> &ndash; Chicago, IL (<a href="https://goo.gl/maps/gDBF9" target="_blank">Map</a>)</p>
+      <p class="show-info">9pm / 21+ / $8 ($10 door)</p>
       <p><a href="http://www.lh-st.com/Shows/11-22-2014+Forgotten+Species" class="btn small">Buy Tix</a></p>
     </div>
 
