bf88ab5bd95478bc999b47564458e9228a62c535
Brad Sawicki <git@bradsawicki.com>
2015-11-08 15:01:59 +0100

    Compass update / Show: Subt Nov 27

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
 
 # COMPASS
-gem 'compass', '~> 1.0.1'
+gem 'compass'
--- views/body.php
+++ views/body.php
@@ -1,20 +1,22 @@
 <section class="main">
   <div class="wrapper">
 
-    <iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=1446188036/size=large/bgcol=000000/linkcol=a80023/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://ancientfriends.bandcamp.com/album/boat">Boat by Ancient Friends</a></iframe>    
-    
+    <iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=1446188036/size=large/bgcol=000000/linkcol=a80023/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://ancientfriends.bandcamp.com/album/boat">Boat by Ancient Friends</a></iframe>
+
     <h2>New EP: <em>Boat</em></h2>
     <p class="album-links"><a href="https://ancientfriends.bandcamp.com/album/boat">Download</a></p>
 <!--     <p class="album-links"><a href="https://ancientfriends.bandcamp.com/album/power-mountain">Download</a> / <a href=" https://itunes.apple.com/us/album/power-mountain-ep/id939746794">iTunes</a> / <a href=" http://open.spotify.com/album/7G30izATkl5R714ajAlmSn">Spotify</a></p> -->
-  
-    <h3>Next Show</h3>    
+
+    <h3>Next Show</h3>
     <div class="show">
-      <h4>July 3</h4>
-      <p class="show-info"><a href="http://emptybottle.com/show/6021505/" target="_blank">Empty Bottle</a></p>
-      <p class="show-info">Chicago, IL (<a href="https://goo.gl/maps/dALy0" target="_blank">Map</a>)</p>
-      <p class="show-info">9pm / 21+ / $5</p>
-      <p>w/ The Pact, HIGHS</p>
-      <p><a href="http://www.ticketweb.com/t3/sale/SaleEventDetail?dispatch=loadSelectionData&eventId=6021505&REFERRAL_ID=twfb&pl=eb" class="btn small">Buy Tickets</a></p>
+      <h4>Friday, November 27</h4>
+      <p class="show-info">
+        <a href="https://www.ticketfly.com/purchase/event/1002471" target="_blank">Subterranean</a>
+      </p>
+      <p class="show-info">Chicago, IL (<a href="https://goo.gl/maps/hzhTwLrYYQ92" target="_blank">Map</a>)</p>
+      <p class="show-info">9pm / 21+ / $8</p>
+      <p>w/ North By North, Hand Practices, The Giving Moon</p>
+      <p><a href="https://www.ticketfly.com/purchase/event/1002471" class="btn small">Buy Tickets</a></p>
     </div>
 
   </div>
